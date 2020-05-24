cbb13f2ae7254887104d2fee0d68106c9754d2cf
TeddyPrograde <teddyprograde@gmail.com>
2020-05-24 18:44:32 -0400

    maybe?

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,7 @@
-const Discord = require('discord.js');
-
 module.exports = {
   name: 'ping',
   description: 'pings bot',
   execute(message, args){
-    message.reply('Pog');
+    message.reply("Pog");
   }
 }

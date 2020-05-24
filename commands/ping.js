b572225a6dccccf15a2bd8d4ff75e23a95a43e9d
TeddyPrograde <teddyprograde@gmail.com>
2020-05-24 18:20:22 -0400

    test command

---
+++ commands/ping.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'ping',
+  description: 'pings bot',
+  execute(message, args){
+    message.channel.send('Pog');
+  }
+}
--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 require('dotenv');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-
 const token = (process.env.TOKEN);
+const PREFIX = "v!"
+
+const fs = require ('fs');
+bot.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+  const command = require(`./commands/${file}`);
+
+  bot.commands.set(command.name, command);
+}
 
 bot.on('ready', () => {
   console.log('thotBot started');
-})
+});
+
+bot.on('message', message => {
+  let args = message.content.substring(PREFIX.length);
+
+  switch (args[0]) {
+
+    case "ping":
+      bot.commands.get('ping').execute(message.args);
+    break;
+
+  }
+});
 
 bot.login(token);

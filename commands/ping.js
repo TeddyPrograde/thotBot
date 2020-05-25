3bdeedfd7a27493dba77d2d49f284f503eef1108
TeddyPrograde <teddyprograde@gmail.com>
2020-05-24 23:07:52 -0400

    whops

--- commands/ping.js
+++ commands/ping.js
@@ -6,7 +6,7 @@ module.exports = {
   execute(message, args){
 
     const embed = new Discord.MessageEmbed()
-    .addTitle('Pong!')
+    .setTitle('Pong!')
     .setColor(0xff2a68)
     message.channel.send(embed);
 

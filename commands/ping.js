b8a58e19a851549e1067d6b55f80ec87bebe79a5
TeddyPrograde <teddyprograde@gmail.com>
2020-05-24 23:06:40 -0400

    Changed pong to a title

--- commands/ping.js
+++ commands/ping.js
@@ -6,7 +6,7 @@ module.exports = {
   execute(message, args){
 
     const embed = new Discord.MessageEmbed()
-    .addField('Pong!')
+    .addTitle('Pong!')
     .setColor(0xff2a68)
     message.channel.send(embed);
 

2b8fccc8ff0bc678a861a14b2a935e0761e822ba
TeddyPrograde <teddyprograde@gmail.com>
2020-06-07 22:57:55 -0400

    Added Moderation Commands to help

--- commands/help.js
+++ commands/help.js
@@ -14,5 +14,14 @@ module.exports = {
     .setFooter(message.member.user.tag)
     message.channel.send(embed);
 
+    const embed2 = new Discord.MessageEmbed()
+    .setColor(0xff2a68)
+    .setAuthor('thotBot', 'https://imgur.com/CB81zbW.jpg')
+    .setTitle('Moderation')
+    .addField('WIP', 'Development of moderation commands is in progress, please be patient.')
+    .setTimestamp()
+    .setFooter(message.member.user.tag)
+    message.channel.send(embed2);
+
   }
 }

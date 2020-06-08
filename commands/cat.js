910adebeaa96eaf0e6ea1f86761d3812e8584ee2
TeddyPrograde <teddyprograde@gmail.com>
2020-06-08 15:47:48 -0400

    cat please

--- commands/cat.js
+++ commands/cat.js
@@ -14,13 +14,13 @@ module.exports = {
     let subreddit = reddit[Math.floor(Math.random() * reddit.length)];
 
     const event = randomPuppy.all(subreddit);
-    event.on('data', url => { const catURL = url} )
-
-    const embed = new Discord.MessageEmbed()
-    .setImage(catURL)
-    .setColor(0xff2a68)
-    .setTimestamp()
-    .setFooter(message.member.user.tag)
-    message.channel.send(embed);
+    event.on('data', url => { const catURL = (url) }.then (
+      const embed = new Discord.MessageEmbed()
+      .setImage(catURL)
+      .setColor(0xff2a68)
+      .setTimestamp()
+      .setFooter(message.member.user.tag)
+      message.channel.send(embed);
+    ))
   }
 }

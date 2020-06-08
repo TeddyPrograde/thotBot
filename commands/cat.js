b7e73c39e737ad2c2d1068fd11e07e0c0d403c48
TeddyPrograde <teddyprograde@gmail.com>
2020-06-08 15:44:17 -0400

    oh dear god

--- commands/cat.js
+++ commands/cat.js
@@ -14,13 +14,13 @@ module.exports = {
     let subreddit = reddit[Math.floor(Math.random() * reddit.length)];
 
     const event = randomPuppy.all(subreddit);
-    event.on('data', url =>{
-      const embed = new Discord.MessageEmbed()
-      .setImage(url)
-      .setColor(0xff2a68)
-      .setTimestamp()
-      .setFooter(message.member.user.tag)
-      message.channel.send(embed);
-    });
+    event.on('data', url => { const catURL = (url)})
+
+    const embed = new Discord.MessageEmbed()
+    .setImage(catURL)
+    .setColor(0xff2a68)
+    .setTimestamp()
+    .setFooter(message.member.user.tag)
+    message.channel.send(embed);
   }
 }

6695cf31664c149320bbaa3f82c067ca148f7e40
TeddyPrograde <teddyprograde@gmail.com>
2020-06-09 00:28:25 -0400

    Accidentally broke everything

--- commands/kick.js
+++ commands/kick.js
@@ -10,42 +10,42 @@ module.exports = {
       .setColor(0xf53838)
       .setDescription('You may not kick members')
       message.channel.send(noPermissionsMessage);
-    } return;
-
-    const user = message.mentions.users.first();
-
-    if(user){
-      const member = message.guild.member(user);
-
-      if(member){
-        member.kick().then(() =>{
-          const successMessage = new Discord.MessageEmbed()
-          .setColor(0x26ff64)
-          .setTitle(`${user.tag} has been kicked`)
-          .setTimestamp()
-          .setFooter(message.member.user.tag)
-          message.channel.send(successMessage);
-
-        }).catch(err =>{
-          const errorMessage = new Discord.MessageEmbed()
-          .setColor(0xf53838)
-          .setDescription('An error has occured, please try again')
-          message.channel.send(errorMessage)
-          console.log(err);
-
-        });
-      } else {
-        const noUserMessage = new Discord.MessageEmbed()
-        .setColor(0xf2d027)
-        .setDescription('This user is not in the server')
-        message.channel.send(noUserMessage);
-
-      }
     } else {
-      const noMentionMessage = new Discord.MessageEmbed()
-      .setColor(0xf2d027)
-      .setDescription('Please mention who you want to kick')
-      message.channel.send(noMentionMessage);
+      const user = message.mentions.users.first();
+
+      if(user){
+        const member = message.guild.member(user);
+
+        if(member){
+          member.kick().then(() =>{
+            const successMessage = new Discord.MessageEmbed()
+            .setColor(0x26ff64)
+            .setTitle(`${user.tag} has been kicked`)
+            .setTimestamp()
+            .setFooter(message.member.user.tag)
+            message.channel.send(successMessage);
+
+          }).catch(err =>{
+            const errorMessage = new Discord.MessageEmbed()
+            .setColor(0xf53838)
+            .setDescription('An error has occured, please try again')
+            message.channel.send(errorMessage)
+            console.log(err);
+
+          });
+        } else {
+          const noUserMessage = new Discord.MessageEmbed()
+          .setColor(0xf2d027)
+          .setDescription('This user is not in the server')
+          message.channel.send(noUserMessage);
+
+        }
+      } else {
+        const noMentionMessage = new Discord.MessageEmbed()
+        .setColor(0xf2d027)
+        .setDescription('Please mention who you want to kick')
+        message.channel.send(noMentionMessage);
+      }
     }
   }
 }

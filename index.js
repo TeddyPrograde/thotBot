88b095673a1cc5ffa40b45147cf1e9b6f8aac014
TeddyPrograde <teddyprograde@gmail.com>
2020-07-29 23:05:31 -0400

    Reverted

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PREFIX = "p!";
 const fs = require ('fs');
 bot.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/'.filter(file => file.endsWith('.js')));
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
   const command = require(`./commands/${file}`);
 
